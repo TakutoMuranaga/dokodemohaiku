abf146898aef84271c711ea02013303eaf7af7db
TakutoMuranaga <takutomura2019@gmail.com>
2022-10-12 06:29:43 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 顧客用
+  # URL /Users/sign_in ...
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    resources :users, only: [:index, :edit, :update, :destroy, :show]
+    resources :poems, only: [:index,:show,:edit,:create,:destroy,:update]
+  end
 end
