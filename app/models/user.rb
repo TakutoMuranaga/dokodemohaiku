a5a651c55698afa7e069a78dd01db0238a360330
TakutoMuranaga <takutomura2019@gmail.com>
2022-10-24 23:05:06 +0000

    フォロー機能途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,17 @@ class User < ApplicationRecord
   has_many :poems, dependent: :destroy
   has_many :poem_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -16,7 +24,7 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -24,10 +32,23 @@ class User < ApplicationRecord
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
-  
+
   #　退会したアカウントがログインできない制約をかける
   def active_for_authentication?
     super && (is_deleted == false)
   end
-  
+
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
